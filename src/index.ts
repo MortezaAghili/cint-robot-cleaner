d3ebdbc4badc03a55f2b36b3606744244c9bc4ff
TheGeekyDev <morteza@thegeeky.dev>
2020-11-18 02:58:32 +0330

    Added move robot function based on start position and instructions

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,7 @@ import { getNumberOfCommands } from "./getNumberOfCommands";
 import { getRobotMoves } from "./getRobotMoves";
 import { getStartPosition } from "./getStartPosition";
 import { ICommand } from "./interfaces/ICommand";
+import { moveTheRobot } from "./MoveRobot";
 
 // utils
 import { Greeting } from "./utils/greeting";
@@ -17,5 +18,18 @@ import { Greeting } from "./utils/greeting";
   const startPosition: Array<number> = await getStartPosition();
 
   // Read robot moves instructions
-  const robotMovesInstructions: Array<ICommand> = await getRobotMoves(numberOfCommands);
+  const robotMovesInstructions: Array<ICommand> = await getRobotMoves(
+    numberOfCommands
+  );
+
+  // Start Cleaning the office and calculate total cleaned places
+  const totalCleanedPlaces = moveTheRobot(
+    startPosition,
+    robotMovesInstructions
+  );
+
+  console.log(`=> Cleaned: ${totalCleanedPlaces}`);
+
+  // Stop reading data from the user
+  process.exit();
 })();
