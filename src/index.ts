554b50157f24f93cc048f26ff084f63e19d80024
TheGeekyDev <morteza@thegeeky.dev>
2020-11-18 02:00:57 +0330

    Add core functions to main function of the app

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,16 @@
+// core functions
+import { getNumberOfCommands } from "./getNumberOfCommands";
+import { getStartPosition } from "./getStartPosition";
+
+// utils
 import { Greeting } from "./utils/greeting";
 
 (async function main() {
   Greeting();
+
+  // Read number of commands
+  const numberOfCommands: number = await getNumberOfCommands();
+
+  // Read start position
+  const startPosition: Array<number> = await getStartPosition();
 })();
